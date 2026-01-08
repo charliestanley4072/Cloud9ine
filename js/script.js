ec52efb18c1f4a33a034e5c7f7982d58fe893142
charliestanley4072 <charliestanley4072@gmail.com>
2026-01-08 21:47:22 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,12 +27,40 @@ function showSuggestions() {
         dublin: "images/dublin-thumbnail.png",
         cork: "images/cork-thumbnail.png",
         liverpool: "images/liverpool-thumbnail.png",
-        isleofman: "images/isleofman-thumbnail.png",
+        isle_of_man: "images/isleofman-thumbnail.png",
 		nottingham: "images/nottingham-thumbnail.png",
 		dover: "images/dover-thumbnail.png",
 		cambridge: "images/cambridge-thumbnail.png",
-		norwich: "images/norwich-thumbnail.png",
-		harpenden: "images/harpenden-thumbnail.png"
+		norwich: "images/norwich-thumbnail.png", // Last main location
+		harpenden: "images/harpenden-thumbnail.png",
+		luton: "images/luton-thumbnail.png",
+		st_albans: "images/stalbans-thumbnail.png",
+		redbourn: "images/redbourn-thumbnail.png",
+		hathersage: "images/hathersage-thumbnail.png",
+		manchester: "images/manchester-thumbnail.png",
+		ware: "images/ware-thumbnail.png",
+		sheffield: "images/sheffield-thumbnail.png",
+		doncaster: "images/doncaster-thumbnail.png",
+		thorne: "images/thorne-thumbnail.png",
+		blackpool: "images/blackpool-thumbnail.png",
+		brighton: "images/brighton-thumbnail.png",
+		cromer: "images/cromer-thumbnail.png",
+		hunstanton: "images/hunstanton-thumbnail.png",
+		holt: "images/holt-thumbnail.png",
+		sherringham: "images/sherringham-thumbnail.png",
+		cornwall: "images/cornwall-thumbnail.png",
+		devon: "images/devon-thumbnail.png",
+		great_yarmouth: "images/great_yarmouth-thumbnail.png",
+		weybourne: "images/weybourne-thumbnail.png",
+		milton_keynes: "images/milton_keynes-thumbnail.png",
+		dunstable: "images/dunstable-thumbnail.png",
+		hatfield: "images/hatfield-thumbnail.png",
+		welwyn_garden_city: "images/welwyn_garden_city-thumbnail.png",
+		radlett: "images/radlett-thumbnail.png",
+		london_colney: "images/london_colney-thumbnail.png",
+		hendon: "images/hendon-thumbnail.png",
+		hemel_hempstead: "images/hemel_hempstead-thumbnail.png",
+		
     };
 
     /* ---------- NORMAL LOCATION MATCHES ---------- */
@@ -45,7 +73,7 @@ function showSuggestions() {
         div.className = "suggestion";
 
         const text = document.createElement("span");
-        text.textContent = loc;
+        text.textContent = loc.replace(/_/g, ' ');
 
         const img = document.createElement("img");
         img.src = locations[loc];
